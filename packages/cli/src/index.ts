32edf665fae99e75019ae559c23eee991dd0948c
Haykel Ben Jemia <hbj@xtech.guru>
2021-08-24 17:09:32 +0100

    COCO-81 Enhance CLI entry code
    Show more meaningful help messages when no
    commands or unknown commands are passed.
    
    Enhance error handling.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,31 @@
-import yargs from 'yargs';
+import yargs, { terminalWidth } from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-yargs(hideBin(process.argv)).strict().commandDir('cmds').demandCommand().help()
-  .argv;
+const parser = yargs(hideBin(process.argv))
+  .commandDir('cmds')
+  .wrap(terminalWidth())
+  .showHelpOnFail(false)
+  .help()
+  .alias('h', 'help')
+  .command({
+    command: '$0', // default command
+    handler: (argv) => {
+      const cmd = argv._[0];
+
+      console.error(
+        cmd ? `Invalid command "${cmd}".` : `Please pass a command.`
+      );
+
+      console.error(`Please run "${argv.$0} help" to get a list of commands.`);
+      process.exit(1);
+    },
+  })
+  .fail(false);
+
+(async function run() {
+  try {
+    await parser.parse();
+  } catch (err) {
+    console.error(`An unexepcted error has occurred: ${err?.message || err}`);
+  }
+})();
