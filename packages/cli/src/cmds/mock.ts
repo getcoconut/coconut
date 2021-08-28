f7655db712b69b4b790294206ccc3b56713e13ff
Haykel Ben Jemia <hbj@xtech.guru>
2021-08-28 09:40:05 +0100

    COCO-81 Enhance error handling for 'mock' command
    Don't catch and log errors in the Pulumi inline program as
    they will be catched and logged by the main app.
    
    Check existence of the Pulumi Cloud project file before
    running the Pulumi inline program to output a friendlier
    error message.

--- packages/cli/src/cmds/mock.ts
+++ packages/cli/src/cmds/mock.ts
@@ -6,6 +6,7 @@ import uniqid = require('uniqid');
 import { LocalWorkspace, OutputMap, Stack } from '@pulumi/pulumi/automation';
 
 import { pulumiProgram } from '../lib/mock/pulumiProgram';
+import { CustomError } from '../lib/errors';
 
 export const command = 'mock [dir]';
 export const desc = 'Mock a Pulumi Cloud project.';
@@ -20,12 +21,17 @@ export const builder = {
 export const handler = async function (argv) {
   const projectPath = path.resolve(argv.dir);
   const projectName = path.basename(projectPath);
+  const projectFile = path.join(projectPath, 'index.ts');
 
   const tempPath = fs.mkdtempSync(
     path.join(os.tmpdir(), `coconut-${projectName}`)
   );
 
-  console.log('Mocking project at', projectPath);
+  console.log(`Mocking project at ${projectFile}...`);
+
+  if (!fs.existsSync(projectFile)) {
+    throw new CustomError('Project file not found.');
+  }
 
   fs.mkdirSync(tempPath, { recursive: true });
 
@@ -38,14 +44,14 @@ export const handler = async function (argv) {
     envVars: {
       PULUMI_CONFIG_PASSPHRASE: uniqid(),
     },
-    program: pulumiProgram(projectPath),
+    program: pulumiProgram(projectFile),
   });
 
   const stack = await Stack.createOrSelect('mock', workspace);
 
   await stack.setConfig('cloud:provider', { value: 'mock' });
 
-  const result = await stack.up({ onOutput: console.info });
+  const result = await stack.up();
 
   console.log(JSON.stringify(unmarshalOutputs(result.outputs), null, 4));
 };
--- packages/cli/src/lib/mock/pulumiProgram.ts
+++ packages/cli/src/lib/mock/pulumiProgram.ts
@@ -1,26 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import path = require('path');
 
-export function pulumiProgram(projectPath: string) {
+export function pulumiProgram(projectFile: string) {
   return async function () {
-    console.log('Running program...');
+    const moduleAlias = require('module-alias');
 
-    try {
-      const moduleAlias = require('module-alias');
+    moduleAlias.addAlias('@pulumi/cloud-mock', '@getcoconut/pulumi-cloud-mock');
 
-      moduleAlias.addAlias(
-        '@pulumi/cloud-mock',
-        '@getcoconut/pulumi-cloud-mock'
-      );
+    require('ts-node').register({ transpileOnly: true });
 
-      require('ts-node').register({ transpileOnly: true });
-
-      const outputs = require(path.join(projectPath, 'index.ts'));
-
-      return outputs;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    return require(projectFile);
   };
 }
