e44ebc9e0f89fa4ee5436b93b73dbe168b42b6b7
Haykel Ben Jemia <hbj@xtech.guru>
2021-10-05 17:55:34 +0200

    COCO-95 Implement the 'outputs' command (WIP)

---
+++ packages/cli/src/lib/cmds/outputs.ts
@@ -0,0 +1,36 @@
+import fs = require('fs-extra');
+import path = require('path');
+import { Command } from 'commander';
+import { LocalProgramArgs, LocalWorkspace } from '@pulumi/pulumi/automation';
+import { CustomError } from '../errors';
+
+export const command = new Command('outputs')
+  .description("write a Pulumi Cloud project's outputs to output targets")
+  .requiredOption('-s, --stack <stack>', 'stack to get output from')
+  .option('-p, --project <dir>', 'project directory', '.')
+  .action(action);
+
+export async function action(options) {
+  const projectPath = path.resolve(options.project);
+
+  const args: LocalProgramArgs = {
+    stackName: options.stack,
+    workDir: projectPath,
+  };
+
+  console.info(`processing outputs of ${projectPath}...`);
+
+  if (!fs.pathExistsSync(projectPath)) {
+    throw new CustomError('project directory does not exist');
+  }
+
+  const stack = await LocalWorkspace.selectStack(args);
+
+  console.info('refreshing stack...');
+  await stack.refresh();
+  console.info('refresh complete');
+
+  const outputs = await stack.outputs();
+
+  console.log(outputs);
+}
